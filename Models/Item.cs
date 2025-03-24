3f9cc8350dde95b4d3aef1f32a8ae09aa88f24e0
Dorsa Moradi <dorsamoradi2016@gmail.com>
2025-03-24 11:25:58 +0100

    Installed packages & added UML diagram

---
+++ Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Warehouse_Management_System.Models;
+
+public partial class Item
+{
+    public int ItemId { get; set; }
+
+    public string ItemName { get; set; } = null!;
+
+    public string? Category { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public virtual ICollection<Stock> Stocks { get; set; } = new List<Stock>();
+}
---
+++ Models/WarehouseManagementSystemContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Warehouse_Management_System.Models;
+
+public partial class WarehouseManagementSystemContext : DbContext
+{
+    public WarehouseManagementSystemContext()
+    {
+    }
+
+    public WarehouseManagementSystemContext(DbContextOptions<WarehouseManagementSystemContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    public virtual DbSet<Stock> Stocks { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=Dorsa\\SQLEXPRESS;Database=WarehouseManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasKey(e => e.ItemId).HasName("PK__Items__727E83EB6117AAA3");
+
+            entity.Property(e => e.ItemId).HasColumnName("ItemID");
+            entity.Property(e => e.Category).HasMaxLength(50);
+            entity.Property(e => e.ItemName).HasMaxLength(100);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<Stock>(entity =>
+        {
+            entity.HasKey(e => e.StockId).HasName("PK__Stock__2C83A9E264E374E5");
+
+            entity.ToTable("Stock");
+
+            entity.Property(e => e.StockId).HasColumnName("StockID");
+            entity.Property(e => e.ItemId).HasColumnName("ItemID");
+            entity.Property(e => e.ReceivedDate).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+            entity.HasOne(d => d.Item).WithMany(p => p.Stocks)
+                .HasForeignKey(d => d.ItemId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Stock__ItemID__3C69FB99");
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.Stocks)
+                .HasForeignKey(d => d.SupplierId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Stock__SupplierI__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Supplier>(entity =>
+        {
+            entity.HasKey(e => e.SupplierId).HasName("PK__Supplier__4BE666944602D1D2");
+
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.ContactPerson).HasMaxLength(100);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(20);
+            entity.Property(e => e.SupplierName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
